a67f3ed308d0e3245683953322672ffffb6e3630
Alexis Lopez <alg9105@gmail.com>
2018-06-12 11:50:24 -0500

    se encontro un error en manage.py que no estaba escrito bien__name__ == __main__

--- manage.py
+++ manage.py
@@ -3,8 +3,11 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 migrate = Migrate(app, db)
+
 manager = Manager(app)
 
-manager.add_command('db',MigrateCommand)
-if __name__ == '__name__':
+manager.add_command('db', MigrateCommand)
+
+if __name__ =='__main__':
     manager.run()
+    
